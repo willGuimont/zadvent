45df9055bd4731e560fd64f70c9395648b3bb3a8
William Guimont-Martin <wil9876543210@hotmail.com>
2025-12-05 18:42:27 -0500

    day 01

--- src/2025/day01.zig
+++ src/2025/day01.zig
@@ -3,15 +3,41 @@ const std = @import("std");
 var buf1: [2048]u8 = undefined;
 
 pub fn part1(input: []const u8) ![]const u8 {
-    var count: usize = 0;
-    var i: usize = 0;
-    while (i < input.len) : (i += 1) {
-        if (input[i] == '\n') count += 1;
+    var dial: i32 = 50;
+    var numZeros: i32 = 0;
+    var it = std.mem.splitAny(u8, input, "\n");
+    while (it.next()) |s| {
+        if (s.len == 0) continue;
+        const sign: i32 = switch (s[0]) {
+            'L' => -1,
+            'R' => 1,
+            else => return error.InvalidInput,
+        };
+        const n = try std.fmt.parseInt(i32, s[1..], 10);
+        dial += n * sign;
+        dial = @mod(dial, 100);
+        if (dial == 0) {
+            numZeros += 1;
+        }
     }
-    return std.fmt.bufPrint(&buf1, "{d}", .{count}) catch "error";
+    return std.fmt.bufPrint(&buf1, "{d}", .{numZeros}) catch "error";
 }
 
 pub fn part2(input: []const u8) ![]const u8 {
-    _ = input;
-    return "a";
+    var dial: i32 = 50;
+    var numZeros: i32 = 0;
+    var it = std.mem.splitAny(u8, input, "\n");
+    while (it.next()) |s| {
+        if (s.len == 0) continue;
+        const sign: i32 = switch (s[0]) {
+            'L' => -1,
+            'R' => 1,
+            else => return error.InvalidInput,
+        };
+        const n = try std.fmt.parseInt(i32, s[1..], 10);
+        dial += sign * n;
+        numZeros += @intCast(@abs(@divFloor(dial, 100)));
+        dial = @mod(dial, 100);
+    }
+    return std.fmt.bufPrint(&buf1, "{d}", .{numZeros}) catch "error";
 }
